d44403d6eddbc5feb773b2170d4d71c3b9bb1061
ZanIhsan2 <rasyid1234rismawan@gmail.com>
2026-08-18 00:40:56 +0700

    belajar membuat api crud sederhana

--- belajarLumen/routes/web.php
+++ belajarLumen/routes/web.php
@@ -12,6 +12,7 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
+use Illuminate\Http\Request;
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
@@ -19,9 +20,45 @@ $router->get('/', function () use ($router) {
 
 $router->group(['prefix' => 'penjualan'], function () use ($router) {
     $router->get('/', function () {
-        return response()->json(['msg' => 'Berhasil get penjualan']);
+        return response()->json([
+            [
+                "id" => 1,
+                "nomor" => "SALE/001",
+                "customer" => "Budi"
+            ],
+            [
+                "id" => 2,
+                "nomor" => "SALE/0012",
+                "customer" => "Mulyon"
+            ],
+        ]);
     });
     $router->get('/{id}', function ($id) {
-        return response()->json(['msg' => 'Berhasil get penjualan dengan id ' . $id]);
+        return response()->json(['data' => [
+                "id" => 2,
+                "nomor" => "SALE/0012",
+                "customer" => "Mulyon",
+                "total" => 1000000,
+                "alamat" => "Bandung"
+            ]]);
+    });
+    $router->post('/', function() {
+        return response()->json([
+            'msg' => 'berhasil',
+            'data' => 4
+        ]);
+    });
+    $router->put('/{id}', function (Request $request, $id) {
+        $nomor = $request->input('nomor');
+        return response()->json(['data' => [
+                "id" => $id,
+                "nomor" => "$nomor",
+                "customer" => "Mulyon",
+                "total" => 1000000,
+                "alamat" => "Bandung"
+            ]]);
+    });
+    $router->delete('/{id}', function($id) {
+        return response()->json(['data' => "data berhasil dihapus"]);
     });
 });
